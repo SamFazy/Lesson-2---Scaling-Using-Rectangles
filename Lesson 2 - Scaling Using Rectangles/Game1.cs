4633b1a069551a672a48000d8d7673a79766d2c9
Sam F <samfaz10@hotmail.com>
2021-11-17 13:45:05 -0500

    1

--- Lesson 2 - Scaling Using Rectangles/Game1.cs
+++ Lesson 2 - Scaling Using Rectangles/Game1.cs
@@ -9,6 +9,13 @@ namespace Lesson_2___Scaling_Using_Rectangles
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D rectTexture;
+        Rectangle rectRectangle;
+
+        Texture2D circTexture;
+        Rectangle circRectangle;
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,7 +26,8 @@ namespace Lesson_2___Scaling_Using_Rectangles
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            rectRectangle = new Rectangle(10, 10, 50, 50);
+            circRectangle = new Rectangle(10, 200, 50, 50);
             base.Initialize();
         }
 
@@ -28,6 +36,8 @@ namespace Lesson_2___Scaling_Using_Rectangles
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            rectTexture = Content.Load<Texture2D>("rectangle");
+            circTexture = Content.Load<Texture2D>("circle");
         }
 
         protected override void Update(GameTime gameTime)
@@ -39,13 +49,24 @@ namespace Lesson_2___Scaling_Using_Rectangles
 
             base.Update(gameTime);
         }
-
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
 
+            _spriteBatch.Draw(circTexture, new Rectangle(190, 10, 400, 450), Color.Yellow);
+            _spriteBatch.Draw(rectTexture, new Rectangle(300, 70, 40, 40), Color.White);
+            _spriteBatch.Draw(circTexture, new Rectangle(315, 85, 10, 10), Color.Black);
+            _spriteBatch.Draw(rectTexture, new Rectangle(435, 70, 40, 40), Color.White);
+            _spriteBatch.Draw(circTexture, new Rectangle(450, 85, 10, 10), Color.Black);
+            _spriteBatch.Draw(rectTexture, new Rectangle(270, 200, 240, 20), Color.Black);
+            _spriteBatch.Draw(rectTexture, new Rectangle(270, 190, 20, 20), Color.Black);
+            _spriteBatch.Draw(rectTexture, new Rectangle(490, 190, 20, 20), Color.Black);
+
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
