842b03be05dccf6137c0a63b0ae91e2c09647fdc
Sam F <samfaz10@hotmail.com>
2021-11-17 12:32:40 -0500

    Add project files.

---
+++ Lesson 2 - Scaling Using Rectangles/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_2___Scaling_Using_Rectangles
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
